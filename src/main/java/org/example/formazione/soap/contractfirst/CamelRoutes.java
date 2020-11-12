4e66cc97697bd8bc6b79ff6ea913ec939180eb5e
Davide <davide.falcone276@gmail.com>
2020-11-12 15:34:05 +0100

    Added camel route to extract data from soap request, using xpath. Updated wsdl and blueprint. Added needed dependencies

--- src/main/java/org/example/formazione/soap/contractfirst/CamelRoutes.java
+++ src/main/java/org/example/formazione/soap/contractfirst/CamelRoutes.java
@@ -1,6 +1,14 @@
 package org.example.formazione.soap.contractfirst;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.XPathBuilder;
+import org.apache.camel.component.cxf.DataFormat;
+import org.apache.camel.model.dataformat.JaxbDataFormat;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CamelRoutes extends RouteBuilder {
 
@@ -8,14 +16,33 @@ public class CamelRoutes extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+
+
         from("cxf:bean:personEndpoint")
                 .routeId("cxf:bean:personEndpoint")
                 .convertBodyTo(java.lang.String.class)
-                .log("Boody ${body}")
-                .to("seda:incomingPerson");
+                .log("Boody DOPO CONVERT TO STRING ${body}")
+                .process((exchange -> {
+                    String s1 = exchange.getIn().getBody().toString();
+                    exchange.getIn().setHeader("ssss", s1);
 
-        from("seda:incomingPerson")
-                .routeId("seda:incomingPerson")
+                    XPathBuilder builder = new XPathBuilder("/soapenv:Envelope/soapenv:Body/con:inputPerson/firstName/text()");
+                    HashMap m1 = new HashMap();
+                    m1.put("soapenv", "http://schemas.xmlsoap.org/soap/envelope/");
+                    m1.put("con", "http://contractfirst.soap.formazione.example.org");
+                    builder.setNamespaces(m1);
+                    String val = builder.evaluate(exchange, String.class);
+                    exchange.getIn().setHeader("ProvaNome", val);
+
+                }))
+                .log("headers ${headers}")
+                .setBody(constant("OK"))
+                .log("Boody DOPO SET CONSTANT ${body}")
+                .convertBodyTo(java.lang.String.class)
+                .to("seda:incomingOrders");
+
+        from("seda:incomingOrders")
+                .routeId("seda:incomingOrders")
                 .to("mock:end");
 
 
